57ace2790a66453d84033069fc700fc6a1e7c8ec
johnmaxnew <forevermeticulous@gmail.com>
2021-09-24 16:59:28 +0300

    added views

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({ Key key }) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,7 +13,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(
+          "Proscan",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: (MediaQuery.of(context).size.height) - AppBar().preferredSize.height - kToolbarHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/icon.png"),
+                    foregroundColor: Colors.transparent,
+                    backgroundColor: Colors.transparent,
+                    radius: 150,
+                  )
+                ],
+              ),
+          ),
+        ),
+      ),
     );
   }
 }
